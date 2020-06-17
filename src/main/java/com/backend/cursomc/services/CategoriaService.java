0e11540871627af0d541a6ba7fed1eda980a644b
Eduardo Botelho Junior <eduardo_botelho_7@hotmail.com>
2020-06-17 16:24:08 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/backend/cursomc/services/CategoriaService.java
+++ src/main/java/com/backend/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.backend.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,10 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 	
 	public Categoria find(Integer id) {
 		return repo.findById(id)
